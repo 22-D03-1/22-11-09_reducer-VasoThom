a1cca4b3059d9a64636aa22606eb2c1edad6efa5
Vaso <vasothom@hotmail.com>
2022-11-09 19:39:07 +0100

    completed

--- kitchenstories/src/App.js
+++ kitchenstories/src/App.js
@@ -1,15 +1,56 @@
 import "./App.css";
 import food from "./food.json";
 import Food from "./components/Food";
+import { useReducer } from "react";
+
+const initState = {
+  checkedSteps: [],
+};
+const reducer = (state, action) => {
+  // return state;
+  switch (action.type) {
+    case "checkStep":
+      const stepIndex = state.checkedSteps.indexOf(action.payload);
+      stepIndex >= 0
+        ? (state = {
+            ...state,
+            checkedSteps: [
+              ...state.checkedSteps.filter((el) => el !== action.payload),
+            ],
+          })
+        : (state = {
+            ...state,
+            checkedSteps: [...state.checkedSteps, action.payload],
+          });
+      break;
+    case "reset":
+      state = {
+        ...state,
+        checkedSteps: [],
+      };
+      break;
+    default:
+      console.log("Failed to check step");
+  }
+  return state;
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Kitchen Stories</h1>
         <div>
           {food.map((el) => {
-            return <Food key={el.id} el={el} />;
+            return (
+              <Food
+                key={el.id}
+                food={el}
+                dispatch={dispatch}
+                checkedSteps={state.checkedSteps}
+              />
+            );
           })}
         </div>
       </header>
--- kitchenstories/src/components/Food.js
+++ kitchenstories/src/components/Food.js
@@ -1,14 +1,31 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from "react";
 
-export default function Food({ el }) {
+export default function Food({ food, dispatch, checkedSteps }) {
+  function onChange() {
+    dispatch({ type: "reset" });
+  }
+
   return (
     <div>
-      <h1>{el.id}</h1>
-
-      <h1>{el.name}</h1>
-      <img src={el.picture} />
-      <p> {el.steps}</p>
+      <h1>{food.id}</h1>
+      <h1>{food.name}</h1>
+      <img src={food.picture} />
+      <br />
+      <label>
+        {food.steps.map((step, i) => (
+          <div key={i}>
+            <div
+              className={checkedSteps.includes(i) ? "checked" : null}
+              onClick={() => {
+                dispatch({ type: "checkStep", payload: i });
+              }}
+            >
+              <p> {step}</p>
+            </div>
+          </div>
+        ))}
+      </label>
+      <button onClick={onChange}>Reset</button>
     </div>
   );
 }
